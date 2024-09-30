e668d2c3340dc920d23cf597e7d2d6aef3c2beaf
dkanukov <dkanukov@ozon.ru>
2024-09-30 12:59:43 +0300

    upd colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -7,9 +7,13 @@ vim.cmd.colorscheme("rose-pine") ]]
 
 -- foreground option can be material, mix, or original
 vim.g.gruvbox_material_foreground = "material"
+
 --background option can be hard, medium, soft
 vim.g.gruvbox_material_background = "hard"
+
 vim.g.gruvbox_material_better_performance = 1
+
 -- 0, 1, 2
 vim.g.gruvbox_material_transparent_background = 1
+
 vim.cmd([[colorscheme gruvbox-material]])
