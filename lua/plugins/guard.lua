deb01287756b06f4bcd08bba3161508a6de58df1
dkanukov <dkanukov@ozon.ru>
2024-12-20 13:00:52 +0300

    last

--- lua/plugins/guard.lua
+++ lua/plugins/guard.lua
@@ -11,6 +11,7 @@ return {
 		local ft = require("guard.filetype")
 
 		ft("lua"):fmt("lsp"):append("stylua"):lint("luacheck")
-		ft("typescript,javascript,typescriptreact"):fmt("lsp"):append("prettierd"):lint("eslint_d")
+		ft("typescript,javascript,typescriptreact,css"):fmt("lsp"):append("prettierd"):lint("eslint_d")
+		-- ft("css"):fmt("lsp"):append("eslint_d"):lint("prettierd")
 	end,
 }
