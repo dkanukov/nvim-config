422fc9880dcde30161c021a92b7a26d3ddbb85e6
Kanukov Denis <kanukov.denis@gmail.com>
2025-05-06 13:29:33 +0300

    add file path copy

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -5,6 +5,18 @@ keymap.set("i", "jk", "<ESC>")
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 keymap.set("n", "x", '"_x')
 keymap.set("n", "<leader>tt", vim.cmd.Ex)
+keymap.set("n", "<leader>cpf", function()
+	local path = vim.fn.expand("%:p")
+	vim.fn.setreg("+", path)
+	vim.notify("Copied to clipboard: " .. path, vim.log.levels.INFO, { title = "Path Copy" })
+end)
+keymap.set("n", "<leader>cp", function()
+	local full_path = vim.fn.expand("%:p")
+	local prefix = "/Users/dmkanukov/"
+	local trimmed_path = full_path:gsub("^" .. vim.pesc(prefix), "")
+	vim.fn.setreg("+", trimmed_path)
+	vim.notify("Copied to clipboard: " .. trimmed_path, vim.log.levels.INFO, { title = "Path Copy" })
+end)
 
 --Tabs
 keymap.set("n", "<leader>to", ":tabnew<CR>")
